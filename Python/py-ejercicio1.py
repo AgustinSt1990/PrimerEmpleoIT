536d477a1ce8fe1b62b0c6347445b3e54880d24b
AgustinSt1990 <af.stigliano@gmail.com>
2022-08-06 20:15:28 -0300

    ejercicio4

---
+++ Python/py-ejercicio1.py
@@ -0,0 +1,12 @@
+tu_nombre = 'Agustin'
+saludar = f'Hola, soy {tu_nombre}'
+
+lin_2 = 'Estoy empezando el curso de Python'
+lin_3 = 'Espero aprender mucho!'
+
+if __name__ == '__main__':
+    print (
+        saludar, '\n',\
+        lin_2, '\n',\
+        lin_3
+        )
---
+++ Python/py-ejercicio2.py
@@ -0,0 +1,4 @@
+cadena = '?'
+
+if __name__ == '__main__':
+    print('{} - {}'.format(cadena, type(cadena)))
