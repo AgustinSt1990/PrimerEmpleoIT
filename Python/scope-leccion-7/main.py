b40385c83560491704605d4670cecc730f3fae6f
AgustinSt1990 <af.stigliano@gmail.com>
2022-08-25 10:47:29 -0300

    ejercicio modulos

---
+++ Python/scope-leccion-7/main.py
@@ -0,0 +1,12 @@
+from package.operaciones import sumar, restar, multiplicar, dividir
+
+def main():
+    a = 6
+    b = 3
+    print (sumar(a,b))
+    print (restar(a,b))
+    print (multiplicar(a,b))
+    print (dividir(a,b))
+
+if __name__ == '__main__':
+    main()
