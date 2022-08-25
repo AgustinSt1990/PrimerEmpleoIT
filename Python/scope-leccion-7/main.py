45bae882e8a74922f8036a27ea94782e6ea18319
AgustinSt1990 <af.stigliano@gmail.com>
2022-08-25 11:23:25 -0300

    ejercicio tiempo

--- Python/scope-leccion-7/main.py
+++ Python/scope-leccion-7/main.py
@@ -1,6 +1,8 @@
 from package.operaciones import sumar, restar, multiplicar, dividir
+from package.hora import ir_a_casa
 
 def main():
+    # Ejercicio 1
     a = 6
     b = 3
     print (sumar(a,b))
@@ -8,5 +10,8 @@ def main():
     print (multiplicar(a,b))
     print (dividir(a,b))
 
+    # Ejercicio 2
+    ir_a_casa()
+    
 if __name__ == '__main__':
     main()
---
+++ Python/scope-leccion-7/package/hora.py
@@ -0,0 +1,11 @@
+from datetime import datetime as dt
+
+def ir_a_casa():
+    hour = dt.now().hour
+    minute = dt.now().minute
+    if hour > 19 and minute > 0:
+        return print ('Son más de las 7! A volver a casa')
+    else:
+        hour_r = 19 - hour
+        minute_r = 60 - minute
+        return print (f'Faltan {hour_r} horas y {minute_r} minutos de trabajo')
