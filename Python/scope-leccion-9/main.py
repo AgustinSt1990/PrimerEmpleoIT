d8d811d9ceb026767d9223e524b4416dd895261e
AgustinSt1990 <af.stigliano@gmail.com>
2022-08-29 11:47:33 -0300

    Ejercicio 2 Leccion 9

--- Python/scope-leccion-9/main.py
+++ Python/scope-leccion-9/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 # Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set). 
 # Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.
 
@@ -12,7 +14,24 @@ def ingrese_paises():
 
     return ', '.join(lista_paises)
 
+
+# En este segundo ejercicio, tenéis que crear una aplicación que obtendrá los elementos impares de una lista pasada por parámetro con filter 
+# y realizará una suma de todos estos elementos obtenidos mediante reduce.
+
+def suma_impares(lista):
+    impar = lambda x: x%2 != 0
+    result = filter(impar, lista)
+    
+    sumar = lambda x, y: x + y
+    result = reduce(sumar, list(result))
+    return result
+
+
 if __name__ == '__main__':
 
     # Ejercicio 1
     print (ingrese_paises())
+
+    # Ejercicio 2
+    lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print (suma_impares(lista))
